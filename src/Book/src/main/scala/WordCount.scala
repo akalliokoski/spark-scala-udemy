44c812591347a21fdb79a0b2203645c0b5fcbdfd
Antti Kalliokoski <antti.a.kalliokoski@gmail.com>
2018-10-02 11:54:23 +0300

    Use regular expression for finding words, normalise data

--- src/Book/src/main/scala/WordCount.scala
+++ src/Book/src/main/scala/WordCount.scala
@@ -8,23 +8,26 @@ object WordCount {
  
   /** Our main function where the action happens */
   def main(args: Array[String]) {
-   
+
     // Set the log level to only print errors
     Logger.getLogger("org").setLevel(Level.ERROR)
-    
+
      // Create a SparkContext using every core of the local machine
-    val sc = new SparkContext("local[*]", "WordCount")   
-    
+    val sc = new SparkContext("local[*]", "WordCount")
+
     // Read each line of my book into an RDD
     val input = sc.textFile("../../book.txt")
-    
-    // Split into words separated by a space character
-    val words = input.flatMap(x => x.split(" "))
-    
-    // Count up the occurrences of each word
-    val wordCounts = words.countByValue()
-    
-    // Print the results.
+
+    // Split using a regular expression that extracts words
+    val words = input.flatMap(x => x.split("\\W+"))
+
+    // Normalize everything to lowercase
+    val lowercaseWords = words.map(x => x.toLowerCase())
+
+    // Count of the occurrences of each word
+    val wordCounts = lowercaseWords.countByValue()
+
+    // Print the results
     wordCounts.foreach(println)
   }
   
